9ce60f3f985d2db8d86d4d0486013c3b0648cf6f
vladzabaluev <vladzabaluev014@gmail.com>
2023-12-20 17:51:25 +0300

    Update DataTypes.cs

--- Runtime/DataTypes.cs
+++ Runtime/DataTypes.cs
@@ -121,6 +121,7 @@ namespace OpenAI
         public ChatMessage Delta { get; set; }
         public int? Index { get; set; }
         public string FinishReason { get; set; }
+        public bool LogProbs { get; set; } = false;
     }
 
     public struct ChatMessage
